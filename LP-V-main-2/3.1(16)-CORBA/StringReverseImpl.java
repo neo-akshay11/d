ee891e3a7aabe07643a21e72e8046c64f9c3c11a
Akshay Patil <akshayjp751@gmail.com>
2026-05-06 23:39:40 +0530

    d

--- LP-V-main-2/3.1(16)-CORBA/StringReverseImpl.java
+++ LP-V-main-2/3.1(16)-CORBA/StringReverseImpl.java
@@ -18,3 +18,22 @@ The method returns the reversed string to the client.
 Input : hello
 Output: olleh
 */
+
+
+/*
+
+sudo apt install openjdk-8-jdk
+
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+
+java -version
+
+javac *.java
+
+orbd -ORBInitialPort 1050&                  //Termi1
+java StringReverseServer -ORBInitialPort 1050 -ORBInitialHost localhost         //termi2
+java StringReverseClient -ORBInitialPort 1050 -ORBInitialHost localhost         //termi3
+
+
+*/
